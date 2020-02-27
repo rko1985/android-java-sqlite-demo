1ea7a4025182b13f127495aa7d73f4fab80bbc16
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-27 08:38:13 -1000

    create insert and select query and log to console

--- app/src/main/java/com/myappcompany/russell/databasedemo/MainActivity.java
+++ app/src/main/java/com/myappcompany/russell/databasedemo/MainActivity.java
@@ -1,8 +1,12 @@
 package com.myappcompany.russell.databasedemo;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.security.spec.ECField;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +15,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE,null);
-        myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3))");
+        try {
+
+            SQLiteDatabase sqLiteDatabase = this.openOrCreateDatabase("Events", MODE_PRIVATE, null);
+            sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS events (name VARCHAR, year INT(4))");
+            sqLiteDatabase.execSQL("INSERT INTO events (name, year) VALUES('Milenium', 2000)");
+            sqLiteDatabase.execSQL("INSERT INTO events (name, year) VALUES('Nick started teaching', 2014)");
+
+            Cursor c = sqLiteDatabase.rawQuery("SELECT * FROM events", null);
+
+            int nameIndex = c.getColumnIndex("name");
+            int yearIndex = c.getColumnIndex("year");
+            c.moveToFirst();
+
+            while (c != null){
+                Log.i("Results - name", c.getString(nameIndex));
+                Log.i("Results - year", Integer.toString(c.getInt(yearIndex)));
+
+                c.moveToNext();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
